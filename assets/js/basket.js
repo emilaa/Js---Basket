c4180724f0f94790683015885ef8753866bdc32c
emilaa <emilaa@code.edu.az>
2022-09-05 23:43:48 +0400

    Homework

---
+++ assets/js/basket.js
@@ -0,0 +1,35 @@
+let products = [];
+
+if (localStorage.getItem("products") != null) {
+  products = JSON.parse(localStorage.getItem("products"));
+}
+
+let tableBody = document.querySelector(".table .table-body");
+
+for (const product of products) {
+  tableBody.innerHTML += `<tr>
+    <td><img src="${product.image}" height="100px" width="100px" class="card-img-top" alt="..."></td>
+    <td>${product.name}</td>
+    <td>${product.desc}</td>
+    <td>${product.count}</td>
+    <td><i class="fa-solid fa-circle-xmark"></i></td>
+  </tr>`;
+}
+
+document.querySelector("sup").innerText = getProductsCount(products);
+
+function getProductsCount(items) {
+  let resultCount = 0;
+  for (const item of items) {
+    resultCount += item.count;
+  }
+  return resultCount;
+}
+
+let deleteIcon = document.querySelector(".fa-circle-xmark");
+
+deleteIcon.addEventListener("click", function () {
+
+this.parentNode.parentNode.remove()
+
+});
